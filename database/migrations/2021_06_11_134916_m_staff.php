8779526c54833e4c77b674b3b90cb011276f8959
suzu1995 <suzu.chao@gmail.com>
2021-06-14 22:49:05 +0900

    スタッフテーブルのマイグレーションの作成

--- database/migrations/2021_06_11_134916_m_staff.php
+++ database/migrations/2021_06_11_134916_m_staff.php
@@ -7,22 +7,26 @@ use Illuminate\Support\Facades\Schema;
 class MStaff extends Migration
 {
     /**
-     * Run the migrations.
+     * スタッフテーブルを作成する
      *
      * @return void
      */
     public function up()
     {
-        //
+        Schema::create('m_staff', function (Blueprint $table) {
+            $table->bigIncrements('staff_id');
+            $table->string('staff_name',50);
+            $table->string('staff_password',50);
+        });
     }
 
     /**
-     * Reverse the migrations.
+     * スタッフテーブルを削除する
      *
      * @return void
      */
     public function down()
     {
-        //
+        Schema::drop('m_staff');
     }
 }
