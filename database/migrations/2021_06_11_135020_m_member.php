1dd168effcf1c2cdcdaf8fc31cf6a9050eee662a
suzu1995 <suzu.chao@gmail.com>
2021-06-16 22:33:06 +0900

    memberテーブル migrationファイル作成

--- database/migrations/2021_06_11_135020_m_member.php
+++ database/migrations/2021_06_11_135020_m_member.php
@@ -13,7 +13,19 @@ class MMember extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('m_member', function (Blueprint $table) {
+            $table->bigIncrements('member_id');
+            $table->timestamp('member_date');
+            $table->string('member_name',50);
+            $table->string('member_password',50);
+            $table->string('member_email',255);
+            $table->integer('member_postal1',5);
+            $table->integer('member_postal2',5);
+            $table->string('member_address',100);
+            $table->integer('member_tel',15);
+            $table->integer('member_sex',1);
+            $table->integer('member_birth',10);
+        });
     }
 
     /**
