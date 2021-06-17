e9ccbd2637195a9932428a6b2e28bda33ba9c37f
suzu1995 <suzu.chao@gmail.com>
2021-06-17 23:52:54 +0900

    詳細テーブルをコミット

--- database/migrations/2021_06_11_135119_t_detail.php
+++ database/migrations/2021_06_11_135119_t_detail.php
@@ -13,7 +13,13 @@ class TDetail extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('t_detail', function (Blueprint $table) {
+            $table->bigIncrements('detail_id');
+            $table->bigInteger('sale_id');
+            $table->bigInteger('product_id');
+            $table->string('product_price',30);
+            $table->integer('product_quantity',15);
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ class TDetail extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('t_detail');
     }
 }
