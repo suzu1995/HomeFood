15b26b505791152677c831bdc4c9092b159dcac2
suzu1995 <suzu.chao@gmail.com>
2021-06-20 21:45:54 +0900

    migrationファイルの修正

--- database/migrations/2021_06_11_135020_m_member.php
+++ database/migrations/2021_06_11_135020_m_member.php
@@ -14,17 +14,19 @@ class MMember extends Migration
     public function up()
     {
         Schema::create('m_member', function (Blueprint $table) {
-            $table->bigIncrements('member_id');
+            $table->primary('member_id');
             $table->timestamp('member_date');
             $table->string('member_name',50);
             $table->string('member_password',50);
             $table->string('member_email',255);
-            $table->integer('member_postal1',5);
-            $table->integer('member_postal2',5);
+            $table->integer('member_postal1',15);
+            $table->integer('member_postal2',15);
             $table->string('member_address',100);
             $table->integer('member_tel',15);
             $table->integer('member_sex',1);
             $table->integer('member_birth',10);
+
+            $table->primary(['member_id']);
         });
     }
 
@@ -35,6 +37,6 @@ class MMember extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('m_member');
     }
 }
