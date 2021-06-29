4f743b25f93713440595836d4dc163b32bf0d03f
suzu1995 <suzu.chao@gmail.com>
2021-06-29 23:30:00 +0900

    共通テンプレートの作成

---
+++ resources/views/common/parent.blade.php
@@ -0,0 +1,11 @@
+<DOCTYPE HTML>
+    <html lang="ja">
+    <head>
+    @yield('head')
+    </head>
+    <body>
+    @yield('header')
+            <p>コンテンツ</p>
+    @yield('footer')
+    </body>
+    </html>
--- resources/views/staff/staff_list.blade.php
+++ resources/views/staff/staff_list.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>スタッフ一覧</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    </head>
+@extends('common.parent')
+@include('common.head')
     <body>
         <div class="bg_pattern Polka_v2"></div>
         <h2>スタッフ一覧画面</h2>
