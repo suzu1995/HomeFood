fc9c8e6a8d92faf5f71dcf804fe8574bf25052b9
suzu1995 <suzu.chao@gmail.com>
2021-07-06 22:17:33 +0900

    新規登録画面作成

--- resources/views/staff/staff_add.blade.php
+++ resources/views/staff/staff_add.blade.php
@@ -5,18 +5,29 @@
     <div class="content-title">
         <h2 class="heading18">スタッフ登録</h2>
     </div><br>
-    <div class="form font-weight-bold h5">
-        <label>名前<br>
-            <input type="text">
-        </label>
-    </div>
-    <div class="form font-weight-bold h5">
-        <label>パスワード<br>
-            <input type="text">
-        </label>
-    </div>
-    <div class="form">
-        <a class="btn btn--kon btn--cubic" href={{route('staff.index')}}>戻る</a>
-        <a class="btn btn--kon btn--cubic" href={{route('staff.store')}}>更新</a>
-    </div>
+
+    <form method="POST" action={{ route('staff.store') }}>
+        {{ csrf_field() }}
+        <div class="form font-weight-bold h5">
+            <label>名前<br>
+                @if($errors->has('staff_name'))
+                    <span class="text-danger">{{ $errors->first('staff_name') }}</span><br>
+                @endif
+                <input type="text" name="staff_name">
+            </label>
+        </div>
+
+        <div class="form font-weight-bold h5">
+            <label>パスワード<br>
+                @if($errors->has('staff_password'))
+                    <span class="text-danger">{{ $errors->first('staff_password') }}</span><br>
+                @endif
+                <input type="password" name="staff_password">
+            </label>
+        </div>
+        <div class="form">
+            <a class="btn btn--kon btn--cubic" href={{ route('staff.index') }}>戻る</a>
+            <input class="btn btn--kon btn--cubic" type="submit" name="add" value="登録">
+        </div>
+    </form>
 </div>
