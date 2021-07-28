b5b069142d2e8785533c1e819cfc73890e51c146
suzu1995 <suzu.chao@gmail.com>
2021-07-28 23:56:10 +0900

    商品一覧画面レイアウト作成

---
+++ resources/views/product/product_list.blade.php
@@ -0,0 +1,56 @@
+@extends('common.parent')
+@include('common.head')
+@include('common.header')
+<div class="content">
+    <div class="content-title">
+        <h2 class="heading18">スタッフ一覧</h2>
+    </div>
+    <a class="btn btn--kon btn--cubic" href="{{ route('product.create') }}">新規登録</a>
+    <table class="info">
+        <thead>
+            <tr>
+                <th></th>
+                <th>商品ID</th>
+                <th>名前</th>
+                <th>値段</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($product_list as $row)
+            <tr>
+                <td><a class="btn btn--kon btn--cubic" href="{{ route('product.edit', $row['product_id']) }}">詳細</a></td>
+                <td>{{$row['product_id']}}</td>
+                <td>{{$row['product_name']}}</td>
+                <td>{{$row['product_price']}}</td>
+            </tr>
+        </tbody>
+        @endforeach
+    </table>
+</div>
+
+<style>
+    table.info > thead > tr > th:nth-child(1),
+    table.info > tbody > tr > td:nth-child(1){
+       width: 60px;
+       text-align: center;
+   }
+
+   table.info > thead > tr > th:nth-child(2),
+   table.info > tbody > tr > td:nth-child(2){
+        width: 80px;
+        text-align: center;
+   }
+
+   table.info > thead > tr > th:nth-child(3),
+   table.info > tbody > tr > td:nth-child(3){
+        width: 250px;
+        text-align: center;
+   }
+
+   table.info > thead > tr > th:nth-child(4){
+        width: 100px;
+   }
+   table.info > tbody > tr > td:nth-child(4){
+        text-align: right;
+   }
+</style>
