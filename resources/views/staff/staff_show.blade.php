94135fa4a0073f1ef61157967e2fc26bd6875d63
suzu1995 <suzu.chao@gmail.com>
2021-08-12 23:59:35 +0900

    スタッフ詳細画面ブレード編集

--- resources/views/staff/staff_show.blade.php
+++ resources/views/staff/staff_show.blade.php
@@ -3,7 +3,7 @@
 @include('common.header')
 <div class="content">
     <div class="content-title">
-        <h2 class="heading18">スタッフ編集</h2>
+        <h2 class="heading18">スタッフ詳細</h2>
     </div><br>
     <a class="btn btn--kon btn--cubic" href="action={{ route('staff.destroy',$staff['staff_id']) }}">このデータを削除する</a>
     <form method="POST" action={{ route('staff.update',$staff['staff_id']) }}>
@@ -19,6 +19,15 @@
                 @endif
             </label>
         </div>
+
+        {{-- <div class="form font-weight-bold h5">
+            <label>現在のパスワード<br>
+                @if($errors->has('current_password'))
+                    <span class="text-danger">{{ $errors->first('current_password') }}</span><br>
+                @endif
+                <input type="password" name="current_password">
+            </label>
+        </div> --}}
         <div class="form font-weight-bold h5">
             <label>パスワード<br>
                 @if($errors->has('staff_password'))
